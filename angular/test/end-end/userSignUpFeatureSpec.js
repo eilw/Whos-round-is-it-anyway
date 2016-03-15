79d184d5a0da4221f25df0ba33a0c63fb47b5451
Eilw <eirikwiig@gmail.com>
2016-03-15 15:54:47 +0000

    added first e2e test

---
+++ angular/test/end-end/userSignUpFeatureSpec.js
@@ -0,0 +1,18 @@
+describe("UserSignUp", function() {
+  it('a user can sign up and be welcomed with their name', function() {
+    browser.get("http://localhost:8000");
+    var userName = element(by.model('userCtrl.userName'));
+    userName.sendKeys('Test Name');
+    var userEmail = element(by.model('userCtrl.email'));
+    userEmail.sendKeys('test@email.com');
+    var userPassword = element(by.model('userCtrl.password'));
+    userPassword.sendKeys('password');
+    var submit = $('#signUp');
+    submit.click();
+    var welcome = element(by.css('h2'));
+    expect(welcome.getText()).toEqual('Welcome, Test Name');
+  });
+
+
+
+});
